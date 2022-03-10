2eab3a2b8b643c67553d8b884f046df4fc32d5d0
kim011206 <63403888+hykk-git@users.noreply.github.com>
2022-03-11 01:50:51 +0900

    [BOJ] / 1541. 잃어버린 괄호 / 브론즈2 / 70분

---
+++ 김희연/1주차/ [BOJ] / 17608 / 브론즈2 / 70분.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class Stick
+{
+public:
+	int arr[64];
+	int num = 2;
+	Stick()
+	{
+		for (int i = 0; i < 64; i++) {
+			arr[i] = 0;
+		}
+		arr[0] = 64;
+	}
+	int sum(int _num)
+	{
+		int count = 0;
+		for (int i = 0; i < _num; i++) {
+			count += arr[i];
+		}
+		return count;
+	}
+	int number() {
+		int count = 0;
+		for (int i = 0; i < 64; i++) {
+			if (arr[i] != 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	int cut(int _X)
+	{
+		int key = 0;
+		while (sum(num)!=_X)
+		{
+			for (int i = 0; i < 64; i++)
+			{
+				arr[i] = (arr[i] / 2);
+				arr[i + 1] = arr[i];
+
+				if ((sum(num)-arr[i]) > _X) 
+				{
+					arr[i] = 0;
+				}
+				if ((sum(num) - arr[i]) == _X)
+				{
+					arr[i] = 0;
+					key = 1;
+					break;
+				}
+				num++;
+			}
+		}
+		return number();
+	}
+};
+
+int main()
+{
+	int X;
+	cin >> X;
+	if (X == 64)
+	{
+		cout << "1";
+	}
+	else
+	{
+		Stick stick;
+		cout << stick.cut(X);
+	}
+}
