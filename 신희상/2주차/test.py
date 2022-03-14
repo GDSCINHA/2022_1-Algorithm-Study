9c87aff4cdd2b88133c40d03d7ff82ed56454536
Ramon K. Shin <51011193+RealKunse@users.noreply.github.com>
2022-03-14 13:00:17 +0900

    [PGS] / 60461. 크레인 인형뽑기 게임 / lv.1 / 45분

--- 신희상/2주차/test.py
+++ 신희상/2주차/test.py
@@ -1,1 +1,16 @@
-print("테스트 파일입니다.")
+def solution(board, moves):
+    picked = []
+    answer = 0
+
+    for j in range(len(moves)):
+        for i in range(len(board)):
+            if board[i][moves[j] - 1] != 0:
+                picked.append(board[i][moves[j] - 1])
+                board[i][moves[j] - 1] = 0
+                break
+
+        if len(picked) >= 2 and picked[-1] == picked[-2]:
+            answer += 2
+            picked = picked[:-2]
+        
+    return answer
