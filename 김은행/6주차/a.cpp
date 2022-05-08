11de6516370f77f35843c3c3990aef44b6782240
Bank <87305109+kimbank@users.noreply.github.com>
2022-05-08 13:32:30 +0900

    [BOJ] 10870. 피보나치 수 5 / 브론즈 2 / 5분

---
+++ 김은행/6주차/a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[13];
+
+
+int main() {
+    arr[0] = 1;
+    
+    int a;
+    cin >> a;
+    
+    for (int i = 1; i <= a; i++) {
+        arr[i] = arr[i - 1] * i;
+    }
+    
+    cout << arr[a] << endl;
+    
+    return 0;
+}
